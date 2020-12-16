dd4d49d85e901e42c15d76ef9270b5455aae98ff
evaempleo <71097900+evaempleo@users.noreply.github.com>
2020-12-15 21:59:13 -0600

    progress

--- contact/main.js
+++ contact/main.js
@@ -1,1 +1,25 @@
 console.log("hello")
+
+ $(document).ready(function() {
+     $('#submit').click(function() {
+     $('#contactform').attr('action',
+                      'mailto:evamarieempleo@gmail.om?subject=Eva Empleo Digital Portfolio' +
+                      $('#name').val() + '&body=' + $('#email').val() + '&body=' + $('#comments').val() + '&body=');
+       $('#contactform').submit();
+     });
+ });
+
+
+ $('#submit').click(function(){
+  //console.log("clicked!")
+  $("#submit").addClass("animate__animated animate__shakeY");
+
+  $(".modal").show();
+
+});
+
+$('.modal').hide();
+
+$('.modal').click(function(){
+  $('.modal').hide();
+})
